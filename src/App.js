db77fd228fcd2c0487a5892dbf47b7346462827f
Ahmed <ahmedali.96@hotmail.com>
2020-08-19 15:52:43 -0400

    responsive ui update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import "./App.css";
 
 import AppState from "./context/AppState";
 import CovidDataSearch from "./components/CovidDataSearch.js";
+import { createMedia } from "@artsy/fresnel";
+
+const AppMedia = createMedia({
+  breakpoints: {
+    mobile: 320,
+    tablet: 768,
+    computer: 992,
+    largeScreen: 1400,
+    widescreen: 1920,
+  },
+});
+
+const mediaStyles = AppMedia.createMediaStyle();
+const { MediaContextProvider } = AppMedia;
 
 const App = () => {
   return (
     <AppState>
+      <style>{mediaStyles}</style>
+
       <div className="App">
         <CovidDataSearch />
 
--- src/components/content/GlobalList.js
+++ src/components/content/GlobalList.js
@@ -2,13 +2,112 @@ import React, { useContext } from "react";
 import AppContext from "../../context/appContext";
 import { Grid } from "semantic-ui-react";
 import CounrtyItem from "../reusable/CountryItem";
+import { createMedia } from "@artsy/fresnel";
+
+const AppMedia = createMedia({
+  breakpoints: {
+    mobile: 320,
+    tablet: 768,
+    computer: 992,
+    largeScreen: 1400,
+    widescreen: 1920,
+  },
+});
+
+const { Media } = AppMedia;
 
 const GlobalList = () => {
   const state = useContext(AppContext);
   const { summaryData } = state;
   return (
     <Grid centered style={{ padding: 15 }}>
-      <Grid.Row centered columns={4}>
+      {/* mobile */}
+
+      <Grid.Row as={Media} at="mobile" centered columns={1}>
+        {summaryData.Countries.map((i, k) => {
+          const addComma = (num) =>
+            num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+          return (
+            <Grid.Column key={k}>
+              <CounrtyItem
+                country={correctedNames(i.Country)}
+                countryCode={i.CountryCode.toLowerCase()}
+                totalConfirmed={addComma(i.TotalConfirmed)}
+                totalRecovered={addComma(i.TotalRecovered)}
+                totalDeaths={addComma(i.TotalDeaths)}
+              />
+            </Grid.Column>
+          );
+        })}
+      </Grid.Row>
+
+      {/* tablet */}
+
+      <Grid.Row as={Media} at="tablet" centered columns={2}>
+        {summaryData.Countries.map((i, k) => {
+          const addComma = (num) =>
+            num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+          return (
+            <Grid.Column key={k}>
+              <CounrtyItem
+                country={correctedNames(i.Country)}
+                countryCode={i.CountryCode.toLowerCase()}
+                totalConfirmed={addComma(i.TotalConfirmed)}
+                totalRecovered={addComma(i.TotalRecovered)}
+                totalDeaths={addComma(i.TotalDeaths)}
+              />
+            </Grid.Column>
+          );
+        })}
+      </Grid.Row>
+
+      {/* computer */}
+
+      <Grid.Row as={Media} at="computer" centered columns={3}>
+        {summaryData.Countries.map((i, k) => {
+          const addComma = (num) =>
+            num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+          return (
+            <Grid.Column key={k}>
+              <CounrtyItem
+                country={correctedNames(i.Country)}
+                countryCode={i.CountryCode.toLowerCase()}
+                totalConfirmed={addComma(i.TotalConfirmed)}
+                totalRecovered={addComma(i.TotalRecovered)}
+                totalDeaths={addComma(i.TotalDeaths)}
+              />
+            </Grid.Column>
+          );
+        })}
+      </Grid.Row>
+
+      {/* large screen */}
+
+      <Grid.Row as={Media} at="largeScreen" centered columns={4}>
+        {summaryData.Countries.map((i, k) => {
+          const addComma = (num) =>
+            num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+          return (
+            <Grid.Column key={k}>
+              <CounrtyItem
+                country={correctedNames(i.Country)}
+                countryCode={i.CountryCode.toLowerCase()}
+                totalConfirmed={addComma(i.TotalConfirmed)}
+                totalRecovered={addComma(i.TotalRecovered)}
+                totalDeaths={addComma(i.TotalDeaths)}
+              />
+            </Grid.Column>
+          );
+        })}
+      </Grid.Row>
+
+      {/* wide screen */}
+
+      <Grid.Row as={Media} at="widescreen" centered columns={6}>
         {summaryData.Countries.map((i, k) => {
           const addComma = (num) =>
             num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
