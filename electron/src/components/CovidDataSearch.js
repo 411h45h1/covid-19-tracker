5bfd71c6e90a3d15ac4b06706623bf8f7c333603
Ahmed <ahmedali.96@hotmail.com>
2020-08-20 14:59:08 -0400

    electron responsive ui update

--- electron/src/components/CovidDataSearch.js
+++ electron/src/components/CovidDataSearch.js
@@ -158,7 +158,73 @@ const CovidDataSearch = () => {
           </Segment>
         </div>
       </Media>
-      <Media greaterThan="tablet">
+      <Media at="computer">
+        <div style={{ padding: 10 }}>
+          <Segment id="Drag" color="brown" inverted loading={isDataLoaded()}>
+            <Header as="h3" textAlign="right">
+              <Button.Group id="NoDrag" floated="right">
+                <Button
+                  as={"a"}
+                  href="https://github.com/AhmedAlihashi/covid-19-tracker"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  content="Click here for the repository"
+                />
+                <Button
+                  icon="power off"
+                  color="red"
+                  onClick={() => {
+                    window.close();
+                  }}
+                />
+              </Button.Group>
+            </Header>
+            <Header style={{ fontSize: "5em" }}>
+              Global Covid-19 Statistics
+            </Header>
+            {summaryData && (
+              <>
+                <CountrySearch />
+
+                <Button
+                  id="NoDrag"
+                  toggle
+                  active={toggleAllCountries}
+                  onClick={() => handleClick()}
+                >
+                  All Countries
+                </Button>
+
+                {!showGlobalList && countrySearch && (
+                  <Segment
+                    style={{
+                      backgroundColor: "#90BEC8",
+                      maxHeight: "60vh",
+                      overflowY: "scroll",
+                    }}
+                  >
+                    <SearchedCountry />
+                  </Segment>
+                )}
+
+                {showGlobalList ? (
+                  <Segment
+                    id="NoDrag"
+                    style={{
+                      backgroundColor: "#90BEC8",
+                      maxHeight: "50vh",
+                      overflowY: "scroll",
+                    }}
+                  >
+                    <GlobalList />
+                  </Segment>
+                ) : null}
+              </>
+            )}
+          </Segment>
+        </div>
+      </Media>
+      <Media greaterThan="computer">
         <div style={{ padding: 5 }}>
           <Segment id="Drag" color="brown" inverted loading={isDataLoaded()}>
             <Header as="h3" textAlign="right">
